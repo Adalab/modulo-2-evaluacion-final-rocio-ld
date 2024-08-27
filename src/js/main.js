450c3202fce3037d5db50be141f7c8a8d2eb892a
rocio_ld <rocio_ld@hotmail.com>
2024-08-27 17:35:06 +0200

    realizando evento al boton

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
+const input = document.querySelector ('.js-textInput');
+const searchbtn = document.querySelector ('.js-search');
+const listdrinks = document.querySelector ('.js.ulList');
+
+/*const renderCharacter= (arrayDrinks)=>{
+    listCocktails.innerHTML='';
+    for(const eachdrink of arrayDrinks){
+        listCocktails
+    }
+}*/
+
+let drinks= [];
+
+/*const getDataApi =()=>{
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`)
+    .then((response)=> response.json())
+    .then((data)=>{
+        console.log(data)
+    })
+
+}*/
+
+const handleSearchCocKtails =()=>{
+    const valueInput= input.value;
+    
+
+
+
+}
+
+searchbtn.addEventListener('click', handleSearchCocKtails);
+
+
+
 
