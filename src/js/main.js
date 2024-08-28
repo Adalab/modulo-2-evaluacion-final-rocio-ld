7ea6612c51f5f0d07249487e070c90bc85d076a9
rocio_ld <rocio_ld@hotmail.com>
2024-08-28 13:35:05 +0200

    funcion seleccionar y añadir a lista de favoritos

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,29 @@
 'use strict';
 
+const main= document.querySelector ('.main');
 const listAnimes = document.querySelector('.js-ulList');
 const divInput = document.querySelector('.divInput');
 const searchbtn = document.querySelector('.js-search');
 const input = document.querySelector('.js-textInput');
+const liEvent=document.querySelector('.liEvent');
+const ulFav= document.querySelector('.ulListFav')
 let animes = [];
+let animesFavorites=[];
 
 
 const  renderAnimes = (arrayAnimes) => {
     listAnimes.innerHTML = '';
+    
     for (const eachAnime of arrayAnimes) {
 
+      
         const liElement = document.createElement('li');
+        liElement.setAttribute('id',eachAnime.mal_id);
+        liElement.setAttribute('class','liEvent')
         listAnimes.appendChild(liElement);
 
         const img = document.createElement('img');
-        //const imageSrc= eachAnime.strDrinkThumb ? eachAnime.strDrinkThumb : 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+        const imageSrc= eachAnime.images.jpg.image_url ? eachAnime.images.jpg.image_url : 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
         img.setAttribute('src', eachAnime.images.jpg.image_url);
         img.setAttribute('alt', eachAnime.title);
         img.setAttribute('class', 'img');
@@ -26,6 +34,36 @@ const  renderAnimes = (arrayAnimes) => {
         h3Title.setAttribute('class', 'h3');
         h3Title.appendChild(h3Text);
         liElement.appendChild(h3Title);
+
+
+        liElement.addEventListener('click',handleFavorites);
+
+    };
+};
+const  renderAnimesFavorites = (arrayAnimes) => {
+    ulFav.innerHTML = '';
+    
+    for (const eachAnime of arrayAnimes) {
+
+      
+        const liElement = document.createElement('li');
+        liElement.setAttribute('id',eachAnime.mal_id);
+        liElement.setAttribute('class','liEvent')
+        ulFav.appendChild(liElement);
+
+        const img = document.createElement('img');
+        const imageSrc= eachAnime.images.jpg.image_url ? eachAnime.images.jpg.image_url : 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+        img.setAttribute('src', eachAnime.images.jpg.image_url);
+        img.setAttribute('alt', eachAnime.title);
+        img.setAttribute('class', 'img');
+        liElement.appendChild(img);
+
+        const h3Title = document.createElement('h3');
+        const h3Text = document.createTextNode(eachAnime.title)
+        h3Title.setAttribute('class', 'h3');
+        h3Title.appendChild(h3Text);
+        liElement.appendChild(h3Title);
+
     };
 };
 
@@ -49,13 +87,33 @@ getDataApi('naruto');
 const handleSearch = () => {
     const searchValue = input.value;
     getDataApi(searchValue);
+};
+
+
+const handleFavorites =(event)=>{
+const id= parseInt(event.currentTarget.id);
+console.log(id);
+//buscar la id a ver si está en el array-> si no está nos devuelve -1, buscar el índice en el array de favoritos
+const indexAnimesFavorite = animesFavorites.findIndex((eachAnime)=> eachAnime.mal_id=== id);
+if (indexAnimesFavorite === -1){
+const animeClickada = animes.find((eachAnime)=> eachAnime.mal_id === id);
+animesFavorites.push(animeClickada);
+}console.log(animesFavorites);
+renderAnimesFavorites(animesFavorites);
+ulFav.classList.remove('hidden');git add
+
 }
 
-
+//Realizar la funcioón manejadora del evento al botón de buscar
 searchbtn.addEventListener('click', handleSearch);
 
 
-//Realizar la funcioón manejadora del evento al botón de buscar
+//Realizar una funcion manejadora para marcar favoritos
+
+
+
+
+
 
 
 
